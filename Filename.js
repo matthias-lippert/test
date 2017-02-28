d1150a39f49d660b3610b0af236e2f734f2a9270
Matthias Lippert <lunatic.white@gmx.de>
2017-02-28 09:05:40 +0100

    erster commit

---
+++ Filename.js
@@ -0,0 +1,8 @@
+//Node.js 6.4
+//Plain Javascript and Node.js is supported
+// html/css is not supported here 
+
+console.log("Hello, Dcoder");
+for(var i=0; i<100; i++) {
+	console.log('ich kann zählen! '+(i+1));
+}
